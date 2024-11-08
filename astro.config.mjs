4ee3775f0cf9e690d794b51746dc9103388aadca
Jose Joya <jose.joya@globant.com>
2024-11-07 21:56:45 -0500

    feat: Added Spanish Documentation to Vixen

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,24 +27,122 @@ export default defineConfig({
       sidebar: [
         {
           label: "📖 Getting Started",
+          translations: {
+            es: "📖 Empieza Aquí",
+          },
           autogenerate: { directory: "getting-started" },
         },
         {
           label: "⭐ Vanilla TS",
-          autogenerate: {
-            directory: "build-application-vanilla",
-            collapsed: true,
+          translations: {
+            es: "⭐ Vanilla TS",
           },
+          items: [
+            {
+              label: "Learn the basics",
+              translations: { es: "Aprende lo básico" },
+              link: "build-application-vanilla/",
+            },
+            {
+              label: "Basic Widgets",
+              translations: {
+                es: "Widgets Básicos",
+              },
+              autogenerate: {
+                directory: "build-application-vanilla/Basic Widgets",
+              },
+              collapsed: true,
+            },
+            {
+              label: "Advanced Widgets",
+              translations: {
+                es: "Widgets Avanzados",
+              },
+              autogenerate: {
+                directory: "build-application-vanilla/Advanced Widgets",
+              },
+              collapsed: true,
+            },
+            {
+              label: "Layout Components",
+              translations: {
+                es: "Componentes Layout",
+              },
+              autogenerate: {
+                directory: "build-application-vanilla/Layout Components",
+              },
+              collapsed: true,
+            },
+            {
+              label: "Main Window and related",
+              translations: {
+                es: "Ventana Principal y Relacionados",
+              },
+              autogenerate: {
+                directory: "build-application-vanilla/Main window and related",
+              },
+              collapsed: true,
+            },
+          ],
         },
         {
           label: "⚛️ React",
-          autogenerate: {
-            directory: "build-application-react",
-            collapsed: true,
+          translations: {
+            es: "⚛️ React.js",
           },
+          items: [
+            {
+              label: "Learn the basics",
+              translations: { es: "Aprende lo básico" },
+              link: "build-application-react/",
+            },
+            {
+              label: "Basic Widgets",
+              translations: {
+                es: "Widgets Básicos",
+              },
+              autogenerate: {
+                directory: "build-application-react/Basic Widgets",
+              },
+              collapsed: true,
+            },
+            {
+              label: "Advanced Widgets",
+              translations: {
+                es: "Widgets Avanzados",
+              },
+              autogenerate: {
+                directory: "build-application-react/Advanced Widgets",
+              },
+              collapsed: true,
+            },
+            {
+              label: "Layout Components",
+              translations: {
+                es: "Componentes Layout",
+              },
+              autogenerate: {
+                directory: "build-application-react/Layout Components",
+              },
+              collapsed: true,
+            },
+            {
+              label: "Main Window and related",
+              translations: {
+                es: "Ventana Principal y Relacionados",
+              },
+              autogenerate: {
+                directory: "build-application-react/Main window and related",
+              },
+              collapsed: true,
+            },
+          ],
         },
         {
           label: "🚧 Development",
+          translations: {
+            es: "Desarrollo",
+          },
           autogenerate: { directory: "development" },
         },
       ],
